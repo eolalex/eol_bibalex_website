df0abcdee238c59d875929549256bbde10d1d603
BA <ba@CDLIN-HICT8169.local.bibalex.dom>
2018-06-20 12:23:14 +0200

    changes in migrations

--- db/migrate/20180619103126_remove_references_from_page_nodes.rb
+++
@@ -1,6 +0,0 @@
-class RemoveReferencesFromPageNodes < ActiveRecord::Migration[5.1]
-  def change
-    remove_reference :page_nodes, :pages, index: true, foreign_key: true
-    remove_reference :page_nodes, :pages, index: true, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180619102743) do
+ActiveRecord::Schema.define(version: 20180619112852) do
 
   create_table "attributions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer "content_id"
