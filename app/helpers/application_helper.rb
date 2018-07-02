623e28805eafafba22bd46b342c42aa212e224e3
BA <nada.eliba@bibalex.org>
2018-07-02 15:50:02 +0200

    Collections Buttons Modified

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
     addclass = options.delete(:button_class) || ""
     haml_tag("button.ui.labeled.small.icon.basic.button.uk-margin-small-bottom#{addclass}") do
       haml_tag("i.#{icon}.icon")
-      haml_concat(link_to(label, url, options))
+      haml_concat(button_to(label, url, options))
     end
   end
 
