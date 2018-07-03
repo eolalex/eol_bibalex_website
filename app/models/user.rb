beffd27b6d4192a80f38c4456d3c86fe2cd1e0dd
BA <ba@CDLIN-HICT8169.local.bibalex.dom>
2018-07-03 09:03:41 +0200

    home, signin and signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,8 +15,8 @@ class User < ApplicationRecord
  
   def password_complexity
     # Regexp extracted from https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
-    return if password.present? and not password.match (/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/)
-    errors.add :password, 'Complexity requirement not met. Password should be at least 8 chacracters long and include: 1 Upper case, 1 lower case, 1 digit and 1 special character'
+    # return if password.present? and not password.match (/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/)
+    # errors.add :password, 'Complexity requirement not met. Password should be at least 8 chacracters long and include: 1 Upper case, 1 lower case, 1 digit and 1 special character'
   end
   
   def can_update?(content_partner)
