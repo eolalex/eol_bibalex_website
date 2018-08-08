da4c6c12cddc6b0ff152e5b07f687def5714ae30
BA <nada.eliba@bibalex.org>
2018-08-08 15:47:39 +0200

    Adding recaptcha to sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable, omniauth_providers: [:facebook, :twitter, :google_oauth2]
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable, :omniauthable, omniauth_providers: [:facebook, :twitter, :google_oauth2]
   validate :username_format, :password_complexity
   validates_confirmation_of :password
   validates :email,presence: true, :uniqueness => true
