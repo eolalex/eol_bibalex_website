d738a9539c990d90136e62d86b9486b3af915956
BA <nada.eliba@bibalex.org>
2018-09-12 15:44:22 +0200

    Some minor edits

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -78,6 +78,6 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    # I18n.locale = params[:locale] || I18n.default_locale
   end
 end
