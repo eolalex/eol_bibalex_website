2a45be99dbf6e13dbe8a620024a33953a9700ca8
root <root@debian>
2019-05-08 10:52:17 +0200

    final working version of user providers

--- app/controllers/user_providers_controller.rb
+++ app/controllers/user_providers_controller.rb
@@ -11,22 +11,23 @@ class UserProvidersController < ApplicationController
     # @user.save!
     else
     @user = User.new(password: password,
-                     username:  params[:info][:name], provider: params[:provider], uid: params[:uid])
+                    username:  params[:info][:name], provider: params[:provider], uid: params[:uid])
    end
+    debugger
     session[:new_user] = params
   end
 
   def create
-    # debugger
+     #debugger
      password = Devise.friendly_token[6,20]
      user = session[:new_user]
      #debugger
      @user = User.new(email: params[:user][:email], password: password, 
                       username: user["info"]["name"], provider: user["provider"], uid: user["uid"])
-     # @user = User.new(email: user["info"]["email"], password: password, 
-                      # username: user["info"]["name"], provider: user["provider"], uid: user["uid"])
+     #@user = User.new(email: user["info"]["email"], password: password, 
+                      #username: user["info"]["name"], provider: user["provider"], uid: user["uid"])
      # return render :new  unless @user.valid?
-    # debugger
+    debugger
      if user["info"]["email"] != params[:user][:email]
        Devise::Mailer.confirmation_instructions(@user, @user.confirmation_token).deliver_now
        flash_msg = I18n.t(:signed_up_but_inactive, scope: 'devise.registrations')
