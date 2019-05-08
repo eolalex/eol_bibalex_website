afa9b4f48d62434ce2a783b12b0c21f206d86a61
root <root@debian>
2019-05-08 13:49:16 +0200

    testing

--- app/controllers/user_providers_controller.rb
+++ app/controllers/user_providers_controller.rb
@@ -13,7 +13,7 @@ class UserProvidersController < ApplicationController
     @user = User.new(password: password,
                     username:  params[:info][:name], provider: params[:provider], uid: params[:uid])
    end
-    debugger
+    #debugger
     session[:new_user] = params
   end
 
@@ -27,7 +27,7 @@ class UserProvidersController < ApplicationController
      #@user = User.new(email: user["info"]["email"], password: password, 
                       #username: user["info"]["name"], provider: user["provider"], uid: user["uid"])
      # return render :new  unless @user.valid?
-    debugger
+    #debugger
      if user["info"]["email"] != params[:user][:email]
        Devise::Mailer.confirmation_instructions(@user, @user.confirmation_token).deliver_now
        flash_msg = I18n.t(:signed_up_but_inactive, scope: 'devise.registrations')
