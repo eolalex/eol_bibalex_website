14af0abeb8aa14c8778c9dad3b68885958abe84c
BA <nada.eliba@bibalex.org>
2019-05-09 08:41:31 +0200

    remove inline js

--- app/assets/javascripts/user_providers_registration_modal.js
+++ app/assets/javascripts/user_providers_registration_modal.js
@@ -1,6 +1,3 @@
 $(document).ready(function() {
-	var username = document.getElementsByName("username")[0].value,
-	    email = document.getElementsByName("email")[0].value;
-	document.getElementById('userName').value = email;
-	$("#hidden_form").submit();
+	$('#popup_OMNI_100').show();
 }); 
