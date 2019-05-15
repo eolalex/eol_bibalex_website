d2d3eaa997738d2642322c793e4c65df7bf3196c
BA <nada.eliba@bibalex.org>
2019-05-15 11:41:39 +0200

    Enhance collection search method

--- app/controllers/collected_pages_controller.rb
+++ app/controllers/collected_pages_controller.rb
@@ -56,27 +56,58 @@ class CollectedPagesController < ApplicationController
   def index
     @collection_id = params[:collection_id]
     @collected_pages = CollectedPage.where(collection_id: @collection_id)
-    @canonical_form = params[:q]
+    @page_title = params[:query]+ "| "+ t(:search_results)
+    regex = ".*"+params[:query].downcase+".*"    
+    page_result = Page.search params[:query] do |body|
+      body[:query] = {
+        regexp:{
+            scientific_name: regex
+        }
+     }
+    end
+    @page_results = page_result.results
 
-    @pages = Page.find_by_sql("select id from pages where node_id in (select node_id from scientific_names where canonical_form like \"%#{@canonical_form}%\")")
-    @pages.each do |p|
-      @collected_pages.each do |collected_page|
-        if collected_page.page.id == p.id
-          if @result.nil?
-            @result = Array.new
+    unless @page_results.empty?
+      # @page_results = @result.paginate( page: params[:page], per_page: ENV['per_page'])
+      @page_results.each do |page_result|
+        @collected_pages.each do |collected_page|
+          if collected_page.page.id == page_result.id
+            if @result.nil?
+              @result = Array.new
+            end
+            @result << page_result
           end
-        @result << p
         end
       end
-    end
-#debugger
-    unless @result.nil?
-      @result = @result.sort_by{|p| Page.find(p.id).scientific_name.downcase}
+     unless @result.nil?
+      @result = @result.sort_by{|page_result| Page.find(page_result.id).scientific_name.downcase}
       @result = @result.paginate( page: params[:page], per_page: ENV['per_page'])
     else
       flash[:notice] = t(:no_results)+" "+ @canonical_form
       redirect_to collection_path(id: @collection_id)
     end
+    end
+    # @canonical_form = params[:q]
+# 
+    # @pages = Page.find_by_sql("select id from pages where node_id in (select node_id from scientific_names where canonical_form like \"%#{@canonical_form}%\")")
+    # @pages.each do |p|
+      # @collected_pages.each do |collected_page|
+        # if collected_page.page.id == p.id
+          # if @result.nil?
+            # @result = Array.new
+          # end
+        # @result << p
+        # end
+      # end
+    # end
+# #debugger
+    # unless @result.nil?
+      # @result = @result.sort_by{|p| Page.find(p.id).scientific_name.downcase}
+      # @result = @result.paginate( page: params[:page], per_page: ENV['per_page'])
+    # else
+      # flash[:notice] = t(:no_results)+" "+ @canonical_form
+      # redirect_to collection_path(id: @collection_id)
+    # end
   end
 
   private
