dce91895e6db4083f8fb9c102de34f44e7dd2505
root <root@debian>
2019-08-28 15:45:26 +0200

    changes on testing server

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -17,21 +17,29 @@ var ValidateForm = function() {
 // }else 
 if((email!= '')&&!(emailRegex).test(email.toLowerCase())){
 	alert("Invalid Email Format");
+        grecaptcha.reset();
 	return false;
 }
 if((password != '')&&(password.length < 6)) {
 alert('Password too short! Must have at least 6 characters');
+grecaptcha.reset();
 return false;
 }else if((password != '') && !(passwordRegex).test(password)){
 	alert("Password Complexity Requirement not met.\nPassword should contain at least 1 uppercase letter, 1 lowercase letter, one special character, and digits");
-	return false;
+	grecaptcha.reset();
+        return false;
 }
 if(password != passwordConfirmation){
 	alert("Password and Password Confirmation Do not Match");
+        grecaptcha.reset();
 	return false;
 } else if(grecaptcha.getResponse().length==0) {
 alert('Please click the reCAPTCHA checkbox');
+grecaptcha.reset();
 return false;
 }
 return true;
 };
+function reset_recaptcha(){
+	grecaptcha.reset();
+}
