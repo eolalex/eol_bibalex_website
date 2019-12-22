4656e8cb8534a76e9ff53c8d188e099a42eb6697
BA <nada.eliba@bibalex.org>
2019-12-22 13:25:26 +0200

    Last Commit using ACL9, switching to another branch to use activeadmin and pundit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
   match '(:anything)' => 'application#nothing', via: [:options]
 
   devise_for :users, only: :omniauth_callbacks, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
